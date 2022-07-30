a82cb53f803f26f16a32436438f6fad7d73e7a3a
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-30 18:03:55 +0300

    Add material UI

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Navbar.css'
-
+import React from "react";
+import "./Navbar.css";
+import { RiLinkedinFill } from "react-icons/ri";
+import SearchIcon from "@mui/icons-material/Search";
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <h2>This is the navbar</h2>
+    <div className="navbar">
+      <h2>This is the navbar</h2>
+      <div className="navbar__left">
+        <RiLinkedinFill />
+        <div className="navbar__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+      <div className="navbar__right"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
