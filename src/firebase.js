db47520a1c8ab11224aceac38d92049cdb12bf70
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-31 21:04:16 +0300

    Add firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUELFiU_PnN2WYBOtKpVBJJYYXMrVNBWI",
+  authDomain: "linkedin-clone-acb35.firebaseapp.com",
+  projectId: "linkedin-clone-acb35",
+  storageBucket: "linkedin-clone-acb35.appspot.com",
+  messagingSenderId: "567098104755",
+  appId: "1:567098104755:web:87346e128378401cacc9c6",
+};
