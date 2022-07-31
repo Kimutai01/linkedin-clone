8e7190f12ba4ab6ceee64bc749e7b482eff84816
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-31 12:35:50 +0300

    Styling feed and inputOptions

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,6 +1,11 @@
 import React from "react";
 import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 import "./Feed.css";
+import InputOption from "./InputOption";
 
 const Feed = () => {
   return (
@@ -13,6 +18,17 @@ const Feed = () => {
             <button type="submit">send</button>
           </form>
         </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#7C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
+        {/* post */}
       </div>
     </div>
   );
